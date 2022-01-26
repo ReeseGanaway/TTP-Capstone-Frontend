fbd32c9d2285106082df844729254a2f2bce0a91
Reese Ganaway <reeseganaway@gmail.com>
2022-01-25 23:56:24 -0500

    Added redux files collectionActions and collectionReducer

---
+++ src/redux/reducers/collectionReducer.js
@@ -0,0 +1,30 @@
+import {
+  FETCH_COLLECTION,
+  START_FETCHING_COLLECTION,
+  END_FETCHING_COLLECTION,
+} from "..actions/collectionActions";
+
+const initialState = {
+  collection: [],
+  isFetchingCollection: true,
+};
+
+export const collectionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_COLLECTION:
+      return {
+        ...state,
+        collection: action.payload,
+      };
+    case START_FETCHING_COLLECTION:
+      return {
+        ...state,
+        isFetchingCollection: true,
+      };
+    case END_FETCHING_COLLECTION:
+      return {
+        ...state,
+        isFetchingCollection: false,
+      };
+  }
+};
